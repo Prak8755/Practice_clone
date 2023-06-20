90e348c1095eb21ca6a209fc394822802e9a1afd
Prakash Singh <prakashsingh@Prakashs-MacBook-Air.local>
2023-06-20 21:13:20 +0530

    Create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 
 import './App.css';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Home from './Pages/Home';
+
+import Profile from './Pages/Profile';
+import SignIn from './Pages/SignIn';
+import SignOut from './Pages/SignOut';
+import Offers from './Pages/Offers';
+import ForgotPassword from './Pages/ForgotPassword';
+
 
 function App() {
   return (
    <>
-   <h1 className='text-blue-400 border'>Hello world</h1>
+   <Router>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/offers' element={<Offers/>}/>
+      <Route path='/forgotPassword' element={<ForgotPassword/>}/>
+      <Route path='/signIn' element={<SignIn/>}/>
+      <Route path='/signOut' element={<SignOut/>}/>
+    </Routes>
+   </Router>
    </>
   );
 }
---
+++ src/Pages/SignOut.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SinOut = () => {
+  return (
+    <div>SinOut</div>
+  )
+}
+
+export default SinOut
